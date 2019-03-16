f919ff2b97cfc696970e402b850c6dea7f93e09b
gokhanpolat <gokhanpolat203@aof.anadolu.edu.tr>
2019-03-16 11:35:33 +0200

    aggregation has been added

---
+++ src/week6/association/aggregation/Manager.java
@@ -0,0 +1,14 @@
+package week6.association.aggregation;
+
+public class Manager {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/week6/association/aggregation/Team.java
@@ -0,0 +1,34 @@
+package week6.association.aggregation;
+
+import java.util.List;
+
+public class Team {
+
+    private Manager manager; // here is aggregation. because Team has-a Manager
+    private String name;
+    private List<String> teamMembers;
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getTeamMembers() {
+        return teamMembers;
+    }
+
+    public void setTeamMembers(List<String> teamMembers) {
+        this.teamMembers = teamMembers;
+    }
+}
