4316003f75deda5f452abb4aa8470e7aee579629
gokhanpolat <gokhanpolat203@aof.anadolu.edu.tr>
2019-03-16 12:24:59 +0200

    enums have been added

--- src/week6/enums/LengthUnit.java
+++ src/week6/enums/LengthUnit.java
@@ -3,9 +3,9 @@ package week6.enums;
 public enum LengthUnit { // enums are set of constant variables
 
     METER(1),
-    DECIMETER(0.1),
-    CENTIMETER(0.01),
-    MILLIMETER(0.001);
+    DECIMETER(10),
+    CENTIMETER(100),
+    MILLIMETER(1000);
 
     LengthUnit(double value) {
         this.value = value;
