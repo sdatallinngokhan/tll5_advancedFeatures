5436ea3f1c7c7d479dd79e31d7774022f2eb837c
gokhanpolat <gokhanpolat203@aof.anadolu.edu.tr>
2019-03-16 12:23:04 +0200

    enums have been added

--- src/week6/enums/EnumTest.java
+++ src/week6/enums/EnumTest.java
@@ -6,5 +6,11 @@ public class EnumTest {
     public static void main(String[] args) {
         System.out.println(LengthUnit.CENTIMETER.getValue());
         System.out.println(WeatherType.RAINY);
+
+        LengthUnit[] lengthUnits = LengthUnit.values();
+
+        for (LengthUnit lengthUnit : lengthUnits) {
+            System.out.println(lengthUnit);
+        }
     }
 }
